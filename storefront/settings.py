2fa97f5c451b9056105b4d18e344a5bad3e57c91
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-04-13 13:27:13 +0500

    managing media files.

--- storefront/settings.py
+++ storefront/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -138,6 +138,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+# url of media.
+MEDIA_ROOT = os.path.join(BASE_DIR,'Media')
+# this represents where the file are store yet.
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- storefront/urls.py
+++ storefront/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 import debug_toolbar
 
@@ -27,4 +29,7 @@ urlpatterns = [
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     path('__debug__/', include(debug_toolbar.urls)),
-]
+] 
+
+if settings.DEBUG: # is true.
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
