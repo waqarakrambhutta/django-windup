3a94f7c691f06a27b7d35e48ac178d24bb8b645b
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-04-17 01:10:54 +0500

    error found

--- store/serializers.py
+++ store/serializers.py
@@ -177,8 +177,7 @@ class ProductImageSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         product_id=self.context['product_id']
         return ProductImage.objects.create(product_id=product_id,**validated_data)
-     
-
+    
     class Meta:
         model = ProductImage
         fields = ['id','image']
--- store/views.py
+++ store/views.py
@@ -144,18 +144,10 @@ class OrderViewSet(ModelViewSet):
 
 class ProductImageViewset(ModelViewSet):
     serializer_class = ProductImageSerializer
-    def create(self, request, *args, **kwargs):
-        serializer = ProductImageSerializer(
-            data=request.data,
-            context={'prodcut_id':self.kwargs['product_pk']}
-            )
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-        
 
-    # def get_serializer_context(self):
-    #     return {'prodcut_id':self.kwargs['product_pk']}
+    def get_serializer_context(self):
+        return {'product_id':self.kwargs['product_pk']}
+        # here's the error...my mistake.
     
     def get_queryset(self):
         return ProductImage.objects.filter(product_id=self.kwargs['product_pk'])
