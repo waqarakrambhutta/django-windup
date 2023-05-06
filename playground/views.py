518d99f321cf05fbf589c6f0d8dfe3087aff9f1f
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-05-06 21:47:47 +0500

    Sending email.

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,10 @@
+from django.core.mail import send_mail,mail_admins,BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
+    try:
+        send_mail('subject','message','waqar@domain.com',['ammar@domain.com'])
+    except BadHeaderError:
+        pass
     return render(request, 'hello.html', {'name': 'Mosh'})
