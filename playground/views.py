2c369091da527a997efe275115e78946e9d65f1c
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-05-07 13:46:28 +0500

    Attaching files.

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,12 @@
-from django.core.mail import send_mail,mail_admins,BadHeaderError
+from django.core.mail import EmailMessage,BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
     try:
-        mail_admins('subject','message',html_message='messages')
-        #mail admin require configuration in the settings.
+        message = EmailMessage('subject','message','waqar@domain.com',['qasim@domain.com'])
+        message.attach_file('playground/static/images/Mosque.jpg')
+        message.send()
     except BadHeaderError:
         pass # we can also show the error message.
     return render(request, 'hello.html', {'name': 'Mosh'})
