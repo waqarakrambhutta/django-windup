bed38ed57da58768d8fb9d9f406ddda7ae4f4905
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-05-14 12:58:56 +0500

    Multiple assertions

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    @pytest.mark.skip
+    # @pytest.mark.skip
     def test_if_user_is_anonymous_return_401(self):
         #Arrange
 
@@ -14,3 +15,29 @@ class TestCreateCollection:
 
         #Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        
+    def test_if_user_is_not_admin_return_403(self):
+        
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/',{'title':'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+        
+    def test_if_data_is_invalid_400(self):
+        
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/',{'title':''})
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['title'] is not None
+    
+    def test_if_data_is_valid_400(self):
+        
+        client = APIClient()
+        client.force_authenticate(user=User(is_staff=True))
+        response = client.post('/store/collections/',{'title':'a'})
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] > 0
