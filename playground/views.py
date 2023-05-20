364952b1336ce9423da4fe4cf747f704fd7c2fbb
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-05-20 13:07:15 +0500

    Using the low-level Cache API

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,12 @@
+from django.core.cache import cache
 from django.shortcuts import render
 import requests
 
 def say_hello(request):
-    requests.get('http://httpbin.org/delay/2')
-    return render(request, 'hello.html', {'name': 'Waqar'})
+    key = 'httpbin_result'  # we can set it anything.
+    if cache.get(key) is None:
+        response = requests.get('http://httpbin.org/delay/2')
+        data = response.json()
+        cache.set(key,data)    # cache.set(key,data, 10 * 60) for setting time to 10 minutes
+
+    return render(request, 'hello.html', {'name': cache.get(key)})
