6df024717ae3b7c078660e52962d82acf74c6624
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-05-20 12:33:55 +0500

    Getting a baseline performance benchmark

--- locustfiles/browse_products.py
+++ locustfiles/browse_products.py
@@ -40,6 +40,10 @@ class WebsiteUser(HttpUser):
             json={'product_id':product_id,'quantity': 1}
         )  
 
+    @task
+    def say_hello(self):
+        self.client.get('/playground/hello/')
+
 
     def on_start(self):
         response = self.client.post('/store/carts/')
