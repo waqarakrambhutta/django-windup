0cfc6ea2f64adf447d5e6a6e85bd4549abd0cb90
Waqar Akram Bhutta <waqarakrambhutta@gmail.com>
2023-06-11 15:45:18 +0500

    SECRET_KEY error

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -5,7 +5,7 @@ from .common import *
 
 DEBUG = False # If debug is off them we need to add the setting allowed hosts below.
 
-SECURITY_KEY = os.environ('SECURITY_KEY')
+SECRET_KEY = os.environ('SECRET_KEY')
 
 ALLOWED_HOSTS = ['mirror-storefront.herokuapp.com']
 
